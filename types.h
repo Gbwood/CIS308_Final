9203495ef3d63cc90081eb0133da174958bd2d8f
Jacob Schilmoeller <schilmjr@cougar.cs.ksu.edu>
2018-12-04 16:15:56 -0600

    Added types

--- types.h
+++ types.h
@@ -8,7 +8,19 @@
 #define types_h
 
 //typedef enum { FALSE, TRUE } bool;
+typedef enum Status{ENABLED, DISABLED} Status;
 typedef enum Color{ BLACK, WHITE, NONE } Color;
+typedef enum State{PLAYER1, PLAYER2} State;
+typedef struct Pair{
+	int row;
+	int col;
+} Pair;
+typedef struct Button{
+	Status current_status;
+	Color color;
+	Pair pair;
+
+} Button;
 char name[81][10];
 int turn;
 GtkWidget *window;
